844902407a9df78bfa738ee2c639eaaa72d236c5
srmadhu <madhuseenapuramrajan@gmail.com>
2023-09-20 15:56:37 +0000

    Packaging changes

--- Package_Client.sh
+++ Package_Client.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -x
 INSTALLDIR=nxt/client/bin/
-mkdir $INSTALLDIR
+rm -rf nxt/client
+mkdir -p $INSTALLDIR
 cp common/lib/libcommon.so $INSTALLDIR
 cp common/lib/libfunctional_1_0.so $INSTALLDIR
-ln -s $INSTALLDIR/libfunctional_1_0.so $INSTALLDIR/libfunctional.so
+OLDPWD=$PWD
+cd $INSTALLDIR
+ln -s ./libfunctional_1_0.so ./libfunctional.so
+cd $OLDPWD
 cp client/bin/UpdateClient $INSTALLDIR
 cp client/scripts/UpdateClient.sh $INSTALLDIR
+tar -cvzf nxt_client.tgz nxt/client
+rm -rf nxt
--- Package_Server.sh
+++ Package_Server.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -x
 INSTALLDIR=nxt/server/bin/
-mkdir $INSTALLDIR
+rm -rf nxt/server
+mkdir -p $INSTALLDIR
 cp common/lib/libcommon.so $INSTALLDIR
 cp common/lib/libfunctional_1_0.so $INSTALLDIR
 cp common/lib/libfunctional_2_0.so $INSTALLDIR
-cp client/bin/UpdateServer $INSTALLDIR
-cp client/scripts/UpdateServer.sh $INSTALLDIR
+cp server/bin/UpdateServer $INSTALLDIR
+cp server/scripts/UpdateServer.sh $INSTALLDIR
+tar -cvzf nxt_server.tgz nxt/server
+rm -rf nxt
